b20286c7945bff02c56dc71091683c85522413b5
Davidm723 <davidmullokandov@gmail.com>
2025-12-20 23:18:34 -0500

    added user service for authenticated requests

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,23 @@
-import { useContext } from "react";
-
+import { useEffect, useContext, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import * as userService from "../../services/userService";
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const fetchedUsers = await userService.index();
+        setUsers(fetchedUsers);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (user) fetchUsers();
+  }, [user]);
 
   return (
     <main>
@@ -11,6 +25,12 @@ const Dashboard = () => {
       <p>
         This is the dashboard page where you can see a list of all the users.
       </p>
+
+      <ul>
+        {users.map((u) => (
+          <li key={u._id}>{u.username}</li>
+        ))}
+      </ul>
     </main>
   );
 };
