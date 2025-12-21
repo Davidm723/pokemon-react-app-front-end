dcd91fd2007b346f284c7d32992e70b5809ba2bc
Davidm723 <davidmullokandov@gmail.com>
2025-12-20 23:06:33 -0500

    added sign out

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,13 +4,23 @@ import { Link } from "react-router";
 import { UserContext } from "../../contexts/UserContext";
 
 const NavBar = () => {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
 
   return (
     <nav>
       {user ? (
         <ul>
           <li>Welcome, {user.username}</li>
+          <li>
+            <Link to="/" onClick={handleSignOut}>
+              Sign Out
+            </Link>
+          </li>
         </ul>
       ) : (
         <ul>
